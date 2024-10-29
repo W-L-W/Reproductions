466a921975ea17563935a09e872f419ad67cb3ac
Lennie Wells <lennie499@gmail.com>
2024-10-29 15:27:18 +0000

    gp from scratch test first success

--- jax_ecosystem/src/gp_from_scratch.py
+++ jax_ecosystem/src/gp_from_scratch.py
@@ -3,6 +3,9 @@
 from abc import ABC, abstractmethod
 
 import jax.numpy as jnp
+import jax.random as r
+import einops as eo
+
 from jaxtyping import Array, Float
 
 
@@ -22,7 +25,7 @@ class Kernel(ABC):
 class RBFKernel(Kernel):
     """RBF Kernel class."""
 
-    def __init__(self, lengthscale: Float[Array, "..."]):
+    def __init__(self, lengthscale: Float):
         self.lengthscale = lengthscale
     
     def matrix(
@@ -30,12 +33,13 @@ class RBFKernel(Kernel):
     ) -> Float[Array, "... N M"]:
         # use trick below but make clearer
         #sqdist = jnp.sum(x1 ** 2, 1).reshape(-1, 1) + jnp.sum(x2 ** 2, 1) - 2 * jnp.dot(x1, x2.T)
+        
         sqdist = (
             - 2 * jnp.einsum('... n d, ... m d -> ... n m', x1, x2)
-            + jnp.einsum('... n d, ... n d -> ... n 1', x1, x1)
-            + jnp.einsum('... m d, ... m d -> ... 1 m', x2, x2)
+            + eo.rearrange(jnp.einsum('... n d, ... n d -> ... n', x1, x1), '... n -> ... 1 n')
+            + eo.rearrange(jnp.einsum('... m d, ... m d -> ... m', x2, x2), '... m -> ... m 1')
         )
-        return jnp.exp(-0.5 / self.lengthscale ** 2 * sqdist)
+        return jnp.exp(-0.5 * sqdist / (self.lengthscale ** 2) )
 
 class ConditionedKernel(Kernel):
     """Kernel obtained from conditioning"""
@@ -60,30 +64,29 @@ class Mean(ABC):
         pass
 
     @abstractmethod
-    def vector(self, x: Float[Array, "... n d"]) -> Float[Array, "... n"]:
+    def vector(self, x: Float[Array, "... N D"]) -> Float[Array, "... N"]:
         """Returns the mean vector."""
         pass
 
 class ZeroMean(Mean):
     """Mean of zero function"""
-    def vector(self.x: Float[Array, "... N D"]) -> Float[Array, "... n"]:
-        return jnp.zeros_like(x)
-
+    def vector(self, x: Float[Array, "... N D"]) -> Float[Array, "... N"]:
+        """Output shape looks like input shape but without the last dimension"""
+        return jnp.zeros(x.shape[:-1])
+    
 class ConditionedMean(Mean):
     """Kernel obtained from conditioning"""
-    def __init__(self, parent_kernel: Kernel, x: Float[Array, "... N D"]):
+    def __init__(self, parent_kernel: Kernel, x: Float[Array, "... N D"], y: Float[Array, "... N"]):
         self.parent_kernel = parent_kernel
-        self.K = parent_kernel.matrix(x,x)
-        self.Km1 = jnp.linalg.inv
+        K = parent_kernel.matrix(x,x)
+        self.Km1y = jnp.linalg.inv(K) @ y
         self.x = x
 
-    def matrix(
-        self, x1: Float[Array, "... N1 D"], x2: Float[Array, "... N2 D"]
+    def vector(
+        self, x1: Float[Array, "... N1 D"]
     ) -> Float[Array, "... N1 N2"]:
-        K12 = self.parent_kernel(x1, x2)
-        K1 = self.parent_kernel(x1, x)
-        K2 = self.parent_kernel(x2, x)
-        return 
+        K1 = self.parent_kernel(x1, self.x)
+        return K1 @ self.Km1y
 
 
 
@@ -95,9 +98,17 @@ class GP:
         self.mean = mean
         self.kernel = kernel
 
+    def sample(self, x: Float[Array, "... N D"], key: r.PRNGKey) -> Float[Array, "... N"]:
+        """Returns a sample from the GP."""
+        mean = self.mean.vector(x)
+        K = self.kernel.matrix(x, x)
+        return r.multivariate_normal(key=key, mean=mean, cov=K, method='svd')
+
     def condition(self, x: Float[Array, "... n d"], y: Float[Array, "... n"]):
         """Returns a GP defined by conditioning"""
-        conditioned_kernel = 
+        conditioned_kernel = ConditionedKernel(self.kernel, x)
+        conditioned_mean = ConditionedMean(self.kernel, x, y)
+        return GP(conditioned_mean, conditioned_kernel)
 
 
 
